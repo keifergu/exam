3bf2801563ce1ee012b07ca76bf1fa1e3149170b
Keifer.Gu <keifergu@gmail.com>
2016-04-17 15:13:01 +0800

    添加网站图标,提供浏览器背景属性,添加离线图标

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('./webpack.config')
-
-var app = new (require('express'))()
+var express = require('express')
+var app = new express()
 var port = 3000
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
-
+app.use(express.static('public'));
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
