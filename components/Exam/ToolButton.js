0a1a23457b396ba2f111551e0a39b180fb0bd7d6
Keifer.Gu <keifergu@gmail.com>
2016-04-26 23:02:37 +0800

    添加底部ToolButton组件

---
+++ components/Exam/ToolButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ToolButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
